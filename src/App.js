61e13e5af0f44596d6a42e1e1830d946165549e1
botvader <eiglesias914@gmail.com>
2022-02-27 13:35:17 +0530

    fix: 🐛 useeffect
    remove useffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import * as Tone from 'tone';
-import { useEffect } from 'react';
 
 function App() {
   const synth = new Tone.PolySynth(Tone.Synth).toDestination();
@@ -15,13 +14,11 @@ function App() {
     'j': 'B4',
     'k': 'C5'
   }
-  useEffect(() => {
-    window.addEventListener('keypress', e => {
-      if(keyMapping[e.key]) {
-        playNote(keyMapping[e.key]);
-      }
-    });
-  }, [])
+  window.addEventListener('keypress', e => {
+    if (keyMapping[e.key]) {
+      playNote(keyMapping[e.key]);
+    }
+  });
 
   function playNote(note) {
     synth.triggerAttackRelease(`${note}`, "5n");
@@ -33,16 +30,16 @@ function App() {
         <ul className='keyboard'>
           <li className='white' onClick={() => playNote('C4')}>C</li>
           <li className='black' onClick={() => playNote('C#4')}>C#</li>
-          <li className='white' style={{margin: '0 0 0 -1em'}} onClick={() => playNote('D4')}>D</li>
+          <li className='white' style={{ margin: '0 0 0 -1em' }} onClick={() => playNote('D4')}>D</li>
           <li className='black' onClick={() => playNote('D#4')}>D#</li>
-          <li className='white' style={{margin: '0 0 0 -1em'}} onClick={() => playNote('E4')}>E</li>
+          <li className='white' style={{ margin: '0 0 0 -1em' }} onClick={() => playNote('E4')}>E</li>
           <li className='white' onClick={() => playNote('F4')}>F</li>
           <li className='black' onClick={() => playNote('F#4')}>F#</li>
-          <li className='white' style={{margin: '0 0 0 -1em'}} onClick={() => playNote('G4')}>G</li>
+          <li className='white' style={{ margin: '0 0 0 -1em' }} onClick={() => playNote('G4')}>G</li>
           <li className='black' onClick={() => playNote('G#4')}>G#</li>
-          <li className='white' style={{margin: '0 0 0 -1em'}} onClick={() => playNote('A4')}>A</li>
+          <li className='white' style={{ margin: '0 0 0 -1em' }} onClick={() => playNote('A4')}>A</li>
           <li className='black' onClick={() => playNote('A#4')}>A#</li>
-          <li className='white' style={{margin: '0 0 0 -1em'}} onClick={() => playNote('B4')}>B</li>
+          <li className='white' style={{ margin: '0 0 0 -1em' }} onClick={() => playNote('B4')}>B</li>
           <li className='white' onClick={() => playNote('C5')}>Ċ</li>
         </ul>
       </header>
